9d68b1e36d89b6f1fc653e4d2c77432bbac0a915
Aaron Smith <smitham50@gmail.com>
2021-05-02 08:23:11 -0400

    add routes and utils and begin basic setup

---
+++ routes/trails.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.router();
+
+router.get('/:slug', (req, res) => {
+
+});
+
+router.post('/search', (req, res) => {
+
+});
+
+router.post('/associate_trails', (req, res) => {
+
+});
+
+router.post('/search_reload', (req ,res) => {
+
+});
